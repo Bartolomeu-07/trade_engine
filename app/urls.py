62f64b0d79c47fa8fd16f589c3d748c66e6a6850
Bartolomeu <bartoszokroj@wp.pl>
2025-09-28 22:26:47 +0200

    Initialize material-kit templates to project

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.views import AssetTypeListView
 
 urlpatterns = [
-    path("assettypes/", AssetTypeListView.as_view(), name="assettypes"),
+    path("assettypes/", AssetTypeListView.as_view(), name="asset-type-list"),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -5,4 +5,6 @@ from app.models import AssetType
 
 class AssetTypeListView(generic.ListView):
     model = AssetType
+    template_name = "app/assettype_list.html"
+    context_object_name = "assettypes"
     fields = "__all__"
