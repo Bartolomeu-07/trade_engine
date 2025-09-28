6ec3d8a1716929e0fb9a3d4ff8b4faf9ff977836
Bartolomeu <bartoszokroj@wp.pl>
2025-09-28 16:51:45 +0200

    Add Investor model inheriting from AbstractUser with additional fields: balance, holdings, is_admin

--- app/models.py
+++ app/models.py
@@ -10,3 +10,9 @@ class Asset(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     type = models.ForeignKey(AssetType, on_delete=models.CASCADE)
+
+
+class Investor(AbstractUser):
+    balance = models.FloatField()
+    holdings = models.ManyToManyField(Asset, related_name='investors')
+    is_admin = models.BooleanField(default=False)
