b4ae462b967b0b7dc4240b507a016806d8f2adb9
Bartolomeu <bartoszokroj@wp.pl>
2025-09-29 17:57:56 +0200

    Add InvestorListView and url

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from app.views import AssetTypeListView, AssetListView
+from app.views import AssetTypeListView, AssetListView, InvestorListView
 
 urlpatterns = [
     path("assettypes/", AssetTypeListView.as_view(), name="asset-type-list"),
     path("assets/", AssetListView.as_view(), name="asset-list"),
+    path("investors/", InvestorListView.as_view(), name="investor-list"),
 ]
 
 app_name = "app"
