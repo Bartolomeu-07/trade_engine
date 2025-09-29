f82ae3eb62a22e472a245f455829110cdb380d45
Bartolomeu <bartoszokroj@wp.pl>
2025-09-29 17:21:45 +0200

    Add view and url for AssetListView

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from app.views import AssetTypeListView
+from app.views import AssetTypeListView, AssetListView
 
 urlpatterns = [
     path("assettypes/", AssetTypeListView.as_view(), name="asset-type-list"),
+    path("assets/", AssetListView.as_view(), name="asset-list"),
 ]
 
 app_name = "app"
--- app/views.py
+++ app/views.py
@@ -8,3 +8,10 @@ class AssetTypeListView(generic.ListView):
     template_name = "app/assettype_list.html"
     context_object_name = "assettypes"
     fields = "__all__"
+
+
+class AssetListView(generic.ListView):
+    model = AssetType
+    template_name = "app/asset_list.html"
+    context_object_name = "assets"
+    fields = "__all__"
