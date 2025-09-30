51d0aa61018dc5f94e04b4e4806ae0b24b994d6f
Bartolomeu <bartoszokroj@wp.pl>
2025-09-30 20:36:41 +0200

    Add Holding model & add max_digits arg to decimal fields

--- app/models.py
+++ app/models.py
@@ -13,13 +13,19 @@ class AssetType(models.Model):
 
 class Asset(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(decimal_places=3)
+    price = models.DecimalField(decimal_places=3, max_digits=10)
     type = models.ForeignKey(AssetType, on_delete=models.CASCADE)
 
 
 class Investor(AbstractUser):
-    balance = models.DecimalField(default=100000, decimal_places=3)
+    balance = models.DecimalField(default=100000, decimal_places=3, max_digits=10)
     holdings = models.ManyToManyField(Asset, related_name='investors')
+
+
+class Holding(models.Model):
+    investor = models.ForeignKey(Investor, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
 
 
 class Order(models.Model):
@@ -33,7 +39,7 @@ class Order(models.Model):
     investor = ForeignKey(Investor, on_delete=models.CASCADE)
     side = models.CharField(max_length=4, choices=SIDE_CHOICES)
     datetime = models.DateTimeField(auto_now_add=True)
-    value = models.DecimalField(editable=False, decimal_places=3)
+    value = models.DecimalField(editable=False, decimal_places=3, max_digits=10)
 
     def save(self, *args, **kwargs):
         self.value = self.asset.price * self.quantity
