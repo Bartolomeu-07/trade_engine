c64b2353982f0a16625532a15a475f6c6455a0f9
Bartolomeu <bartoszokroj@wp.pl>
2025-10-06 19:27:42 +0200

    Update testmodels.py: add setUp method & add test_holding_str_method test

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,10 +1,33 @@
 from django.test import TestCase
 
-from app.models import AssetType
+from app.models import AssetType, Holding, Investor, Asset
 
 
 class TestModels(TestCase):
-    def test_asset_type_str_method(self):
-        asset_type = AssetType.objects.create(name="Stocks")
+    def setUp(self):
+        self.investor = Investor.objects.create(
+            username="TestInvestor",
+            email="testemail@kj.com",
+            password="TestPass123#",
+            balance=10000,
+        )
+        self.asset_type = AssetType.objects.create(name="Stocks")
+        self.asset = Asset.objects.create(
+            name="Tesla",
+            price=3118.20,
+            type=self.asset_type,
+        )
+        self.holding = Holding.objects.create(
+            investor=self.investor,
+            asset=self.asset,
+            quantity=3,
+        )
 
-        self.assertEqual(str(asset_type), asset_type.name)
+    def test_asset_type_str_method(self):
+        self.assertEqual(str(self.asset_type), self.asset_type.name)
+
+    def test_holding_str_method(self):
+        self.assertEqual(
+            str(self.holding),
+            f"{self.investor.username} holds {self.holding.quantity} × {self.asset.name}"
+        )
