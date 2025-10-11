bd3889f1065ce9c21a6d2774ca77fe9f580235c2
Bartolomeu <bartoszokroj@wp.pl>
2025-10-11 17:21:04 +0200

    Update test_views.py: add setUp method with user authorization & add test_home_page

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,4 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+User = get_user_model()
 
 class TestViews(TestCase):
-    pass
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser')
+        self.client.force_login(self.user)
+
+    def test_home_page(self):
+        url = reverse("app:index")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, 'app/index.html')
+
