008b0c182c1cb7d075d3995973fc3acfc6711b11
Bartolomeu <bartoszokroj@wp.pl>
2025-10-11 17:30:49 +0200

    Update test_views.py: add test_asset_type_list

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from app.models import AssetType
+
 User = get_user_model()
 
 class TestViews(TestCase):
@@ -16,4 +18,19 @@ class TestViews(TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertTemplateUsed(res, 'app/index.html')
+
+    def test_asset_type_list(self):
+        AssetType.objects.create(name="Stocks")
+        AssetType.objects.create(name="Crypto")
+        asset_types = AssetType.objects.all()
+
+        url = reverse("app:asset-type-list")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(
+            list(asset_types),
+            list(res.context['assettypes']),
+        )
+        self.assertTemplateUsed(res, 'app/assettype_list.html')
 
