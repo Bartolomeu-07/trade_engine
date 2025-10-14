c97dd8052542644d3d89a150cf8f146fc0fd523b
Bartolomeu <bartoszokroj@wp.pl>
2025-10-14 21:26:15 +0200

    Update test_forms.py: add test_asset_form_type_field

--- app/tests/test_forms.py
+++ app/tests/test_forms.py
@@ -1,5 +1,14 @@
+from django import forms
 from django.test import TestCase
+
+from app.forms import AssetForm
 
 
 class TestForms(TestCase):
-    pass
+    # --AssetForm--
+    def test_asset_form_type_field(self):
+        form = AssetForm()
+        self.assertIn("type", form.fields)
+        field = form.fields["type"]
+        self.assertIsInstance(field, forms.ModelChoiceField)
+        self.assertEqual(field.empty_label, "--Select the type of an asset--")
