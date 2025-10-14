d2babb1be1e7903e31b27b5c69b4a1a67ec80e9f
Bartolomeu <bartoszokroj@wp.pl>
2025-10-14 21:32:00 +0200

    Update test_forms.py: add test_asset_form_with_valid_data

--- app/tests/test_forms.py
+++ app/tests/test_forms.py
@@ -1,7 +1,11 @@
+from decimal import Decimal
+from typing import assert_type
+
 from django import forms
 from django.test import TestCase
 
 from app.forms import AssetForm
+from app.models import AssetType, Asset
 
 
 class TestForms(TestCase):
@@ -12,3 +16,19 @@ class TestForms(TestCase):
         field = form.fields["type"]
         self.assertIsInstance(field, forms.ModelChoiceField)
         self.assertEqual(field.empty_label, "--Select the type of an asset--")
+
+    def test_asset_form_with_valid_data(self):
+        self.asset_type = AssetType.objects.create(name="Forex")
+        data = {
+            "name": "USD/PLN",
+            "price": "4.34",
+            "type": self.asset_type.pk,
+        }
+        form = AssetForm(data=data)
+
+        self.assertTrue(form.is_valid(), form.errors)
+        obj = form.save()
+        self.assertIsInstance(obj, Asset)
+        self.assertEqual(obj.name, "USD/PLN")
+        self.assertEqual(obj.price, Decimal("4.34"))
+        self.assertEqual(obj.type, self.asset_type)
